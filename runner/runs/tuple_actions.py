e12b5c8c184f6bd3bea786a3b56993e6f5fdd09f
Aleksei Petrenko <petrenko@usc.edu>
2019-08-12 19:59:29 -0700

    Experiment params

--- runner/runs/tuple_actions.py
+++ runner/runs/tuple_actions.py
@@ -2,15 +2,13 @@ from runner.run_description import RunDescription, Experiment, ParamGrid
 from runner.run_many import run
 
 _params = ParamGrid([
-    ('env', ['doom_battle_hybrid']),
-    ('rollout', [64, 32]),
     ('recurrence', [1, 16]),
-    ('ppo_clip_value', [0.25, 10.0]),
+    ('ppo_epochs', [1, 4]),
 ])
 
 _experiment = Experiment(
     'doom_tuple_actions',
-    'python -m algorithms.ppo.train_ppo --train_for_seconds=360000 --gamma=0.998 --batch_size=1024 --entropy_loss_coeff=0.0005 --max_grad_norm=8.0 --num_envs=144 --num_workers=18 --hidden_size=512 --env_frameskip=2',
+    'python -m algorithms.ppo.train_ppo --env=doom_battle_hybrid --train_for_seconds=360000 --gamma=0.998 --batch_size=1024 --entropy_loss_coeff=0.0005 --num_envs=144 --num_workers=18 --hidden_size=512 --env_frameskip=2',
     _params.generate_params(randomize=False),
 )
 
