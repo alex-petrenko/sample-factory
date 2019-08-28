3e57dc86fd4f12cb044dc6ccc8c58dfac5b1d006
Aleksei Petrenko <petrenko@usc.edu>
2019-08-27 18:13:13 -0700

    Fixed seed evaluation

---
+++ runner/runs/doom_bots_seeds.py
@@ -0,0 +1,16 @@
+from runner.run_description import RunDescription, Experiment, ParamGrid
+from runner.run_many import run
+
+_params = ParamGrid([
+    ('seed', [42, 42, 42, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]),
+])
+
+_experiment = Experiment(
+    'bots_v20_fs2',
+    'python -m train_pytorch --env=doom_dwango5_bots_experimental --train_for_seconds=360000 --algo=PPO --gamma=0.995 --recurrence=1 --use_rnn=False --env_frameskip=2',
+    _params.generate_params(randomize=False),
+)
+
+gridsearch = RunDescription('doom_bots_v20_fs2_seeds', experiments=[_experiment], pause_between_experiments=10, use_gpus=6, experiments_per_gpu=2, max_parallel=12)
+
+run(gridsearch)
