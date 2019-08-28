636860856ee0df172aaebcdcdfe88ac6e0e1bcf7
Aleksei Petrenko <petrenko@usc.edu>
2019-08-28 14:45:34 -0700

    DMLAB runner settings

--- runner/runs/memory_dmlab.py
+++ runner/runs/memory_dmlab.py
@@ -9,12 +9,13 @@ _params = ParamGrid([
     ('mem_size', [4, 0]),
 ])
 
+# IMPORTANT: for DMLAB number of workers better be equal to the number of envs, otherwise spurious crashes may occur!
 _experiment = Experiment(
     'mem_dmlab_v21',
-    'python -m train_pytorch --algo=PPO --rollout=64 --num_envs=64 --train_for_env_steps=1000000000 --normalize_advantage=False --prior_loss_coeff=0.005',
+    'python -m train_pytorch --algo=PPO --rollout=64 --num_envs=64 --num_workers=64 --train_for_env_steps=1000000000 --normalize_advantage=False --prior_loss_coeff=0.005',
     _params.generate_params(randomize=False),
 )
 
-gridsearch = RunDescription('mem_dmlab_v21', experiments=[_experiment], pause_between_experiments=10, use_gpus=4, experiments_per_gpu=2, max_parallel=8)
+gridsearch = RunDescription('mem_dmlab_v21', experiments=[_experiment], pause_between_experiments=15, use_gpus=4, experiments_per_gpu=2, max_parallel=8)
 
 run(gridsearch)
