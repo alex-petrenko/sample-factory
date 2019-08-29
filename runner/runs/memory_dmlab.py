cb942215af7a4f3ae1ea31fd68b178fc8bf4ef05
Aleksei Petrenko <petrenko@usc.edu>
2019-08-29 15:54:43 -0700

    Update dmlab parameters (reward scale, recurrence, num envs). Version V23.

--- runner/runs/memory_dmlab.py
+++ runner/runs/memory_dmlab.py
@@ -1,19 +1,24 @@
 from runner.run_description import RunDescription, Experiment, ParamGrid
-from runner.run_processes import run
 
 _params = ParamGrid([
-    ('env', ['dmlab_nonmatch', 'dmlab_watermaze']),
-    ('recurrence', [64]),
+    ('env', ['dmlab_nonmatch']),
     ('use_rnn', [True, False]),
-    ('ppo_epochs', [4]),
     ('mem_size', [4, 0]),
+    ('reward_scale', [0.1]),
 ])
 
+cmd = 'python -m train_pytorch --algo=PPO --rollout=64 --recurrence=32 --num_envs=96 --num_workers=96 --train_for_env_steps=1000000000 --normalize_advantage=False --prior_loss_coeff=0.005 '
+
 # IMPORTANT: for DMLAB number of workers better be equal to the number of envs, otherwise spurious crashes may occur!
-_experiment = Experiment(
-    'mem_dmlab_v21',
-    'python -m train_pytorch --algo=PPO --rollout=64 --num_envs=64 --num_workers=64 --train_for_env_steps=1000000000 --normalize_advantage=False --prior_loss_coeff=0.01',
+_experiment_nm = Experiment(
+    'mem_dmlab_nm',
+    cmd + '--reward_scale=0.1 --env=dmlab_nonmatch',
+    _params.generate_params(randomize=False),
+)
+_experiment_wm = Experiment(
+    'mem_dmlab_wm',
+    cmd + '--reward_scale=1.0 --env=dmlab_watermaze',
     _params.generate_params(randomize=False),
 )
 
-RUN_DESCRIPTION = RunDescription('mem_dmlab_v21', experiments=[_experiment])
+RUN_DESCRIPTION = RunDescription('mem_dmlab_v23', experiments=[_experiment_nm, _experiment_wm])
