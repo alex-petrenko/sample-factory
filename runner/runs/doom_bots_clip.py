327194900248ac23a92e786390cc39e430fa80df
Aleksei Petrenko <petrenko@usc.edu>
2019-08-31 19:10:32 -0700

    Do we need clip? experiment

---
+++ runner/runs/doom_bots_clip.py
@@ -0,0 +1,17 @@
+from runner.run_description import RunDescription, Experiment, ParamGrid
+from runner.run_processes import run
+
+_params = ParamGrid([
+    ('ppo_clip_ratio', [1.1, 1000.0]),
+    ('ppo_clip_value', [0.2, 1000.0]),
+    ('ppo_epochs', [1]),
+    ('recurrence', [32]),
+])
+
+_experiment = Experiment(
+    'bots_fs2_clip',
+    'python -m train_pytorch --env=doom_dwango5_bots_experimental --train_for_seconds=360000 --algo=PPO --gamma=0.995 --env_frameskip=2 --use_rnn=True --rollout=64 --num_envs=96 --reward_scale=0.5 --target_kl=1000.0',
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription('doom_bots_v27_fs2_clip', experiments=[_experiment], pause_between_experiments=10, use_gpus=6, experiments_per_gpu=2, max_parallel=12)
