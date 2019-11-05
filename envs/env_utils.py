204f4c700fdc4c40f4dded3934b5351c74fe9877
Aleksei Petrenko <petrenko@usc.edu>
2019-11-04 17:11:14 -0800

    Fixed env initialization bug

--- envs/env_utils.py
+++ envs/env_utils.py
@@ -5,7 +5,7 @@ from utils.utils import log
 
 def create_multi_env(num_envs, num_workers, make_env_func, stats_episodes):
     tmp_env = make_env_func(None)
-    is_multiagent = hasattr(tmp_env, 'num_agents')
+    is_multiagent = hasattr(tmp_env, 'num_agents') and tmp_env.num_agents > 1
 
     if is_multiagent:
         assert num_envs % tmp_env.num_agents == 0
