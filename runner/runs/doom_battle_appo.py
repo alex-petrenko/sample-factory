c71c040dc7c2b45958ac20e6a2da9e7e967b4361
Aleksei Petrenko <petrenko@usc.edu>
2019-12-18 02:02:30 -0800

    Experiment scripts

--- runner/runs/doom_battle_appo.py
+++ runner/runs/doom_battle_appo.py
@@ -1,19 +1,14 @@
 from runner.run_description import RunDescription, Experiment, ParamGrid
 
 _params = ParamGrid([
-    ('seed', [42, 42]),
+    ('seed', [42]),
+    ('prior_loss_coeff', [0.0005, 0.001, 0.002, 0.005]),
 ])
 
-_experiment_32 = Experiment(
+_experiment = Experiment(
     'battle_fs4_32',
     'python -m algorithms.appo.train_appo --env=doom_battle_hybrid --train_for_seconds=360000 --algo=APPO --env_frameskip=4 --use_rnn=True --reward_scale=0.5 --num_workers=20 --num_envs_per_worker=20 --num_policies=1 --ppo_epochs=1 --rollout=32 --recurrence=32 --macro_batch=1024',
     _params.generate_params(randomize=False),
 )
 
-_experiment_16 = Experiment(
-    'battle_fs4_16',
-    'python -m algorithms.appo.train_appo --env=doom_battle_hybrid --train_for_seconds=360000 --algo=APPO --env_frameskip=4 --use_rnn=True --reward_scale=0.5 --num_workers=20 --num_envs_per_worker=20 --num_policies=1 --ppo_epochs=1 --rollout=16 --recurrence=16 --macro_batch=1024',
-    _params.generate_params(randomize=False),
-)
-
-RUN_DESCRIPTION = RunDescription('doom_battle_appo_v47_fs4_speed', experiments=[_experiment_32, _experiment_16], pause_between_experiments=30, use_gpus=4, experiments_per_gpu=1, max_parallel=4)
+RUN_DESCRIPTION = RunDescription('doom_battle_appo_v48_fs4_save', experiments=[_experiment], pause_between_experiments=30, use_gpus=4, experiments_per_gpu=1, max_parallel=4)
