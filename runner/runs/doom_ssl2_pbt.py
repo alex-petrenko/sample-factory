524da7fed46d17bba1f1b071b91c3d3faa619652
Aleksei Petrenko <petrenko@usc.edu>
2020-01-25 00:24:10 -0800

    Experiment script

--- runner/runs/doom_ssl2_pbt.py
+++ runner/runs/doom_ssl2_pbt.py
@@ -6,7 +6,7 @@ _params = ParamGrid([
 
 _experiment = Experiment(
     'bots_ssl2_fs2',
-    'python -m algorithms.appo.train_appo --env=doom_ssl2_duel --train_for_seconds=360000 --algo=APPO --gamma=0.995 --env_frameskip=2 --use_rnn=True --reward_scale=0.5 --num_workers=80 --num_envs_per_worker=20 --num_policies=8 --ppo_epochs=1 --rollout=32 --recurrence=32 --macro_batch=2048 --batch_size=2048 --benchmark=False --pbt_replace_reward_gap=0.3 --pbt_period_env_steps=5000000',
+    'python -m algorithms.appo.train_appo --env=doom_ssl2_duel --train_for_seconds=360000 --algo=APPO --gamma=0.995 --env_frameskip=2 --use_rnn=True --reward_scale=0.5 --num_workers=80 --num_envs_per_worker=12 --num_policies=8 --ppo_epochs=1 --rollout=32 --recurrence=32 --macro_batch=2048 --batch_size=2048 --benchmark=False --pbt_replace_reward_gap=0.3 --pbt_period_env_steps=5000000',
     _params.generate_params(randomize=False),
 )
 
