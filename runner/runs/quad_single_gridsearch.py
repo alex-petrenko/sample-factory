9fe5f8b6531d62276d835a15d294d8c98b2cd7d3
Aleksei Petrenko <petrenko@usc.edu>
2020-04-10 01:02:01 -0700

    Experiment

--- runner/runs/quad_single_gridsearch.py
+++ runner/runs/quad_single_gridsearch.py
@@ -10,7 +10,7 @@ _params = ParamGrid([
 
 _experiment = Experiment(
     'quads_gridsearch',
-    'run_algorithm --env=quadrotor_single --train_for_env_steps=100000000 --algo=APPO --gamma=0.99 --use_rnn=False --num_workers=36 --num_envs_per_worker=8 --num_policies=1 --rollout=32 --recurrence=32 --benchmark=False --with_pbt=False --ppo_clip_ratio=0.05 --value_loss_coeff=1',
+    'run_algorithm --env=quadrotor_single --train_for_env_steps=100000000 --algo=APPO --gamma=0.99 --use_rnn=False --num_workers=36 --num_envs_per_worker=4 --num_policies=1 --rollout=32 --recurrence=32 --benchmark=False --with_pbt=False --ppo_clip_ratio=0.05 --value_loss_coeff=1',
     _params.generate_params(randomize=False),
 )
 
