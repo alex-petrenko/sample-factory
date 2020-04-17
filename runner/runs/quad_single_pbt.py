c0f88d2989f2d07f178037e9a4f830535b190d7f
Aleksei Petrenko <petrenko@usc.edu>
2020-04-16 23:43:28 -0700

    Fixed experiment

--- runner/runs/quad_single_pbt.py
+++ runner/runs/quad_single_pbt.py
@@ -16,7 +16,7 @@ _params = ParamGrid([
 
 _experiment = Experiment(
     'quads_pbt',
-    'algorithms.appo.train_appo --env=quadrotor_single --train_for_seconds=3600000 --algo=APPO --gamma=0.99 --use_rnn=True --num_workers=72 --num_envs_per_worker=8 --num_policies=8 --ppo_epochs=1 --rollout=32 --recurrence=32 --benchmark=False --pbt_replace_reward_gap=0.1 --pbt_replace_reward_gap_absolute=20.0 --pbt_period_env_steps=1000000 --pbt_start_mutation=10000000 --pbt_optimize_batch_size=True --with_pbt=True',
+    'algorithms.appo.train_appo --env=quadrotor_single --train_for_seconds=3600000 --algo=APPO --gamma=0.99 --use_rnn=False --num_workers=72 --num_envs_per_worker=10 --num_policies=8 --ppo_epochs=1 --rollout=32 --recurrence=32 --benchmark=False --pbt_replace_reward_gap=0.1 --pbt_replace_reward_gap_absolute=20.0 --pbt_period_env_steps=1000000 --pbt_start_mutation=10000000 --pbt_optimize_batch_size=True --with_pbt=True',
     _params.generate_params(randomize=False),
 )
 
