32c99397137b532c2a7e47c62d425102d711d220
Tushar K <tushartk2095@gmail.com>
2020-05-31 18:56:04 -0700

    Readded setup.py and removed fast_queue references from it

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    # Information
+    name='sample-factory',
+    version='0.0.1',
+    url='https://github.com/alex-petrenko/sample-factory',
+    author='Aleksei Petrenko',
+    license='MIT',
+    keywords='asynchronous reinforcement learning policy gradient ppo impala',
+
+    # Build instructions
+    setup_requires=['setuptools>=45.2.0', 'cython>=0.29'],
+)
