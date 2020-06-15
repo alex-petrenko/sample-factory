76906c85ec4199e306d7a3d1cc097a3952e4287b
Tushar K <tushartk2095@gmail.com>
2020-06-15 10:08:02 -0700

    Refactored tests to make them work

--- runner/runs/doom_battle_appo_pbt.py
+++ runner/runs/doom_battle_appo_pbt.py
@@ -5,7 +5,7 @@ _params = ParamGrid([])
 _experiments = [
     Experiment(
         'battle_fs4_pbt',
-        'algorithms.appo.train_appo --env=doom_battle --train_for_env_steps=300000000000 --algo=APPO --env_frameskip=4 --use_rnn=True --ppo_epochs=1 --rollout=32 --recurrence=32 --macro_batch=2048 --batch_size=2048 --wide_aspect_ratio=False --num_workers=72 --num_envs_per_worker=36 --num_policies=12 --pbt_replace_reward_gap=0.1 --pbt_replace_reward_gap_absolute=5.0 --pbt_period_env_steps=5000000 --pbt_start_mutation=100000000 --reset_timeout_seconds=300 --with_pbt=True',
+        'python -m algorithms.appo.train_appo --env=doom_battle --train_for_env_steps=300000000000 --algo=APPO --env_frameskip=4 --use_rnn=True --ppo_epochs=1 --rollout=32 --recurrence=32 --macro_batch=2048 --batch_size=2048 --wide_aspect_ratio=False --num_workers=72 --num_envs_per_worker=36 --num_policies=12 --pbt_replace_reward_gap=0.1 --pbt_replace_reward_gap_absolute=5.0 --pbt_period_env_steps=5000000 --pbt_start_mutation=100000000 --reset_timeout_seconds=300 --with_pbt=True',
         _params.generate_params(randomize=False),
     ),
 ]
--- runner/runs/quad_single_lr.py
+++ runner/runs/quad_single_lr.py
@@ -11,7 +11,7 @@ _params_earlystop = ParamGrid([
 
 _experiment_earlystop = Experiment(
     'quads_gridsearch_earlystop',
-    'run_algorithm --env=quadrotor_single --train_for_env_steps=1000000000 --algo=APPO --gamma=0.99 --use_rnn=False --num_workers=24 --num_envs_per_worker=2 --num_policies=1 --rollout=700 --recurrence=1 --benchmark=False --with_pbt=False --ppo_clip_ratio=0.05 --batch_size=128 --nonlinearity=tanh --actor_critic_share_weights=False --policy_initialization=xavier_uniform --adaptive_stddev=False --hidden_size=256 --with_vtrace=False --max_policy_lag=100000000 --gae_lambda=1.00 --device=cpu --max_grad_norm=0.0 --num_minibatches_to_accumulate=0',
+    'python -m run_algorithm --env=quadrotor_single --train_for_env_steps=1000000000 --algo=APPO --gamma=0.99 --use_rnn=False --num_workers=24 --num_envs_per_worker=2 --num_policies=1 --rollout=700 --recurrence=1 --benchmark=False --with_pbt=False --ppo_clip_ratio=0.05 --batch_size=128 --nonlinearity=tanh --actor_critic_share_weights=False --policy_initialization=xavier_uniform --adaptive_stddev=False --hidden_size=256 --with_vtrace=False --max_policy_lag=100000000 --gae_lambda=1.00 --device=cpu --max_grad_norm=0.0 --num_minibatches_to_accumulate=0',
     _params_earlystop.generate_params(randomize=False),
 )
 
