b3a83df0172377846b0aa98c839abc2e9c9d5647
Tushar K <tushartk2095@gmail.com>
2020-07-14 00:53:39 -0700

    Retry loop addition (#30)
    * Added new version of retry loop
    
    * Edited multiagent wrapper to have retry functions
    
    * Changed retry function definition
    
    * Incorportaed review comments

--- envs/env_utils.py
+++ envs/env_utils.py
@@ -1,4 +1,6 @@
-from utils.utils import is_module_available
+from utils.utils import is_module_available, log
+from functools import wraps
+from time import sleep
 
 
 class EnvCriticalError(Exception):
@@ -19,3 +21,20 @@ def quadrotors_available():
 
 def dmlab_available():
     return is_module_available('deepmind_lab')
+
+
+def retry(exception_class=Exception, num_attempts=3, sleep_time=1):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for i in range(num_attempts):
+                try:
+                    return func(*args, **kwargs)
+                except exception_class as e:
+                    if i == num_attempts - 1:
+                        raise
+                    else:
+                        log.error('Failed with error %r, trying again', e)
+                        sleep(sleep_time)
+        return wrapper
+    return decorator
