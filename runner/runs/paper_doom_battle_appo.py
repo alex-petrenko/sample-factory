3086c5b2e24a9059f651da27c928b849653dccf2
Aleksei Petrenko <petrenko@usc.edu>
2020-08-09 04:13:58 -0700

    Experiment

--- runner/runs/paper_doom_battle_appo.py
+++ runner/runs/paper_doom_battle_appo.py
@@ -13,4 +13,4 @@ _experiments = [
 ]
 
 
-RUN_DESCRIPTION = RunDescription('paper_doom_battle_appo_v103_fs4', experiments=_experiments)
+RUN_DESCRIPTION = RunDescription('paper_doom_battle_appo_v104_fs4', experiments=_experiments)
