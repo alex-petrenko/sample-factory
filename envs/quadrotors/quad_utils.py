72d32d2b0d7225e88290e51ae56e37ec8414865b
Zhehui Huang <30305127+Zhehui-Huang@users.noreply.github.com>
2020-08-28 23:08:16 -0700

    Minor changes of env quadrotors (#50)

--- envs/quadrotors/quad_utils.py
+++ envs/quadrotors/quad_utils.py
@@ -11,7 +11,7 @@ def make_quadrotor_env_single(cfg, **kwargs):
     quad = 'Crazyflie'
     dyn_randomize_every = dyn_randomization_ratio = None
 
-    episode_duration = 7  # seconds
+    episode_duration = cfg.quads_episode_duration  # seconds
 
     raw_control = raw_control_zero_middle = True
 
@@ -52,7 +52,7 @@ def make_quadrotor_env_multi(cfg, **kwargs):
     quad = 'Crazyflie'
     dyn_randomize_every = dyn_randomization_ratio = None
 
-    episode_duration = 7  # seconds
+    episode_duration = cfg.quads_episode_duration  # seconds
 
     raw_control = raw_control_zero_middle = True
 
@@ -67,7 +67,7 @@ def make_quadrotor_env_multi(cfg, **kwargs):
     dynamics_change = dict(noise=dict(thrust_noise_ratio=0.05), damp=dict(vel=0, omega_quadratic=0))
 
     env = QuadrotorEnvMulti(
-        num_agents=4,
+        num_agents=cfg.quads_num_agents,
         dynamics_params=quad, raw_control=raw_control, raw_control_zero_middle=raw_control_zero_middle,
         dynamics_randomize_every=dyn_randomize_every, dynamics_change=dynamics_change, dyn_sampler_1=sampler_1,
         sense_noise=sense_noise, init_random_state=True, ep_time=episode_duration, rew_coeff=rew_coeff,
