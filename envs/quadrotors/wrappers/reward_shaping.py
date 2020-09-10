628614d6823dba824308315d1cf53b5892ed7901
Zhehui Huang <30305127+Zhehui-Huang@users.noreply.github.com>
2020-09-10 16:48:21 -0700

    Choose scenarios by using command line, minor modifications (#53)

--- envs/quadrotors/wrappers/reward_shaping.py
+++ envs/quadrotors/wrappers/reward_shaping.py
@@ -4,7 +4,7 @@ import numpy as np
 
 DEFAULT_QUAD_REWARD_SHAPING = dict(
     quad_rewards=dict(
-        pos=1.0, effort=0.05, spin=0.1, vel=0.0, crash=1.0, orient=1.0, yaw=0.0,
+        pos=1.0, effort=0.05, spin=0.1, vel=0.0, crash=1.0, orient=1.0, yaw=0.0, quadcol_bin=0., quadsettle=0.,
     ),
 )
 
