1432ff907801df7054b5eb38060b0194b7687afd
Aleksei Petrenko <petrenko@usc.edu>
2020-09-13 03:18:56 -0700

    Experiments

--- runner/runs/voxel_slurm_pbt.py
+++ runner/runs/voxel_slurm_pbt.py
@@ -6,8 +6,8 @@ _params = ParamGrid([
 
 _experiment = Experiment(
     'voxel_env_pbt',
-    'python -m algorithms.appo.train_appo --env=voxel_env_v12 --train_for_seconds=360000000 --algo=APPO --gamma=0.995 --use_rnn=True --num_envs_per_worker=16 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --actor_worker_gpus 0 1 2 3 4 5 6 7 --num_policies=8 --with_pbt=True --max_grad_norm=0.0 --pbt_replace_reward_gap_absolute=0.2 --exploration_loss=symmetric_kl --exploration_loss_coeff=0.001 --num_agents=4 --vertical_look_limit=0.2 --pbt_mix_policies_in_one_env=False',
+    'python -m algorithms.appo.train_appo --env=voxel_env_v17_v --train_for_seconds=360000000 --algo=APPO --gamma=0.998 --use_rnn=True --num_workers=56 --num_envs_per_worker=2 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --actor_worker_gpus 0 1 2 3 4 5 6 7 --num_policies=8 --with_pbt=True --max_grad_norm=0.0 --pbt_replace_reward_gap_absolute=0.2 --exploration_loss=symmetric_kl --exploration_loss_coeff=0.001 --num_agents=4 --vertical_look_limit=0.2 --pbt_mix_policies_in_one_env=False --use_vulkan=True',
     _params.generate_params(randomize=False),
 )
 
-RUN_DESCRIPTION = RunDescription('voxel_env_pbt_v107_env_v12_8p', experiments=[_experiment])
+RUN_DESCRIPTION = RunDescription('voxel_env_pbt_v108_env_v17_8p', experiments=[_experiment])
