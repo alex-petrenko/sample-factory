38b98b572f319a213827d92c95d13fa059089c83
Aleksei Petrenko <petrenko@usc.edu>
2020-09-16 15:15:32 -0700

    Faster conv encoder with torch.jit.script

--- algorithms/appo/model_utils.py
+++ algorithms/appo/model_utils.py
@@ -129,6 +129,41 @@ class EncoderBase(nn.Module):
 
 
 class ConvEncoder(EncoderBase):
+    class ConvEncoderImpl(nn.Module):
+        """
+        After we parse all the configuration and figure out the exact architecture of the model,
+        we devote a separate module to it to be able to use torch.jit.script (hopefully benefit from some layer
+        fusion).
+        """
+        def __init__(self, activation, conv_filters, fc_layer_size, encoder_extra_fc_layers, obs_shape):
+            super(ConvEncoder.ConvEncoderImpl, self).__init__()
+            conv_layers = []
+            for layer in conv_filters:
+                if layer == 'maxpool_2x2':
+                    conv_layers.append(nn.MaxPool2d((2, 2)))
+                elif isinstance(layer, (list, tuple)):
+                    inp_ch, out_ch, filter_size, stride = layer
+                    conv_layers.append(nn.Conv2d(inp_ch, out_ch, filter_size, stride=stride))
+                    conv_layers.append(activation)
+                else:
+                    raise NotImplementedError(f'Layer {layer} not supported!')
+
+            self.conv_head = nn.Sequential(*conv_layers)
+            self.conv_head_out_size = calc_num_elements(self.conv_head, obs_shape.obs)
+
+            fc_layers = []
+            for i in range(encoder_extra_fc_layers):
+                size = self.conv_head_out_size if i == 0 else fc_layer_size
+                fc_layers.extend([nn.Linear(size, fc_layer_size), activation])
+
+            self.fc_layers = nn.Sequential(*fc_layers)
+
+        def forward(self, obs):
+            x = self.conv_head(obs)
+            x = x.contiguous().view(-1, self.conv_head_out_size)
+            x = self.fc_layers(x)
+            return x
+
     def __init__(self, cfg, obs_space, timing):
         super().__init__(cfg, timing)
 
@@ -145,29 +180,18 @@ class ConvEncoder(EncoderBase):
         else:
             raise NotImplementedError(f'Unknown encoder {cfg.encoder_subtype}')
 
-        conv_layers = []
-        for layer in conv_filters:
-            if layer == 'maxpool_2x2':
-                conv_layers.append(nn.MaxPool2d((2, 2)))
-            elif isinstance(layer, (list, tuple)):
-                inp_ch, out_ch, filter_size, stride = layer
-                conv_layers.append(nn.Conv2d(inp_ch, out_ch, filter_size, stride=stride))
-                conv_layers.append(nonlinearity(cfg))
-            else:
-                raise NotImplementedError(f'Layer {layer} not supported!')
+        activation = nonlinearity(self.cfg)
+        fc_layer_size = fc_after_encoder_size(self.cfg)
+        encoder_extra_fc_layers = self.cfg.encoder_extra_fc_layers
 
-        self.conv_head = nn.Sequential(*conv_layers)
-        self.conv_head_out_size = calc_num_elements(self.conv_head, obs_shape.obs)
-        log.debug('Convolutional layer output size: %r', self.conv_head_out_size)
+        enc = self.ConvEncoderImpl(activation, conv_filters, fc_layer_size, encoder_extra_fc_layers, obs_shape)
+        self.enc = torch.jit.script(enc)
 
-        self.init_fc_blocks(self.conv_head_out_size)
+        self.encoder_out_size = calc_num_elements(self.enc, obs_shape.obs)
+        log.debug('Encoder output size: %r', self.encoder_out_size)
 
     def forward(self, obs_dict):
-        x = self.conv_head(obs_dict['obs'])
-        x = x.contiguous().view(-1, self.conv_head_out_size)
-
-        x = self.forward_fc_blocks(x)
-        return x
+        return self.enc(obs_dict['obs'])
 
 
 class ResBlock(nn.Module):
