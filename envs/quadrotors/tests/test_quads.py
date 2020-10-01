a2f5401d2c76f2bec8e4db044e7ed67b7ecd30b8
Tushar K <tushartk2095@gmail.com>
2020-09-30 23:04:13 -0700

    Added enabling of numba with flag in sample factory

--- envs/quadrotors/tests/test_quads.py
+++ envs/quadrotors/tests/test_quads.py
@@ -5,7 +5,30 @@ from algorithms.utils.arguments import default_cfg
 from envs.create_env import create_env
 from envs.env_utils import quadrotors_available
 from utils.timing import Timing
-from utils.utils import log
+from utils.utils import log, is_module_available
+
+
+def numba_available():
+    return is_module_available('numba')
+
+
+def run_multi_quadrotor_env(env_name, cfg):
+    env = create_env(env_name, cfg=cfg)
+    env.reset()
+    for i in range(100):
+        obs, r, d, info = env.step([env.action_space.sample() for _ in range(env.num_agents)])
+
+    n_frames = 1000
+    env = create_env(env_name, cfg=cfg)
+    env.reset()
+
+    timing = Timing()
+    with timing.timeit('step'):
+        for i in range(n_frames):
+            obs, r, d, info = env.step([env.action_space.sample() for _ in range(env.num_agents)])
+
+    log.debug('Time %s, FPS %.1f', timing, n_frames * env.num_agents / timing.step)
+    env.close()
 
 
 class TestQuads(TestCase):
@@ -34,20 +57,13 @@ class TestQuads(TestCase):
         env_name = 'quadrotor_multi'
         cfg = default_cfg(env=env_name)
         self.assertIsNotNone(create_env(env_name, cfg=cfg))
+        run_multi_quadrotor_env(env_name, cfg)
 
-        env = create_env(env_name, cfg=cfg)
-        env.reset()
-        for i in range(100):
-            obs, r, d, info = env.step([env.action_space.sample() for _ in range(env.num_agents)])
-
-        n_frames = 5000
-        env = create_env(env_name, cfg=cfg)
-        env.reset()
-
-        timing = Timing()
-        with timing.timeit('step'):
-            for i in range(n_frames):
-                obs, r, d, info = env.step([env.action_space.sample() for _ in range(env.num_agents)])
-
-        log.debug('Time %s, FPS %.1f', timing, n_frames * env.num_agents / timing.step)
-        env.close()
+    @unittest.skipUnless(quadrotors_available(), 'quadrotor env package is not installed')
+    @unittest.skipUnless(numba_available(), 'Numba is not installed')
+    def test_quad_multi_env_with_numba(self):
+        env_name = 'quadrotor_multi'
+        cfg = default_cfg(env=env_name)
+        cfg.quads_use_numba = True
+        self.assertIsNotNone(create_env(env_name, cfg=cfg))
+        run_multi_quadrotor_env(env_name, cfg)
