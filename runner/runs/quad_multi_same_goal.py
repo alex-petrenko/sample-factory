a5435229f92a4934bccfd36a99ec616f4cd6d077
Sumeet <sscifi@gmail.com>
2020-10-26 07:53:35 -0700

    revert changes to runner script

--- runner/runs/quad_multi_same_goal.py
+++ runner/runs/quad_multi_same_goal.py
@@ -16,4 +16,15 @@ _experiment = Experiment(
     _params.generate_params(randomize=False),
 )
 
-RUN_DESCRIPTION = RunDescription('quads_multi_same_goal_collision_gridsearch_v112', experiments=[_experiment])
+RUN_DESCRIPTION = RunDescription('quads_multi_same_goal_v112', experiments=[_experiment])
+
+# On Brain server, when you use num_workers = 72, if the system reports: Resource temporarily unavailable,
+# then, try to use two commands below
+# export OMP_NUM_THREADS=1
+# export USE_SIMPLE_THREADED_LEVEL3=1
+
+# Command to use this script on server:
+# xvfb-run python -m runner.run --run=quad_multi_same_goal --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
+# Command to use this script on local machine:
+# Please change num_workers to the physical cores of your local machine
+# python -m runner.run --run=quad_multi_same_goal --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
