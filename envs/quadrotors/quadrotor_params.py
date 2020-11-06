eb0f1897fab54406a55bbad000dae7bac172dc9e
Sumeet Batra <sscifi@gmail.com>
2020-11-05 22:28:26 -0800

    parent 027e0a611bd4d11fbfce0d2f7e6a03db60d96e11 (#70)
    add support for lissajous curves for the goals

--- envs/quadrotors/quadrotor_params.py
+++ envs/quadrotors/quadrotor_params.py
@@ -23,7 +23,7 @@ def add_quadrotors_env_args(env, parser):
     p.add_argument('--quads_collision_reward', default=None, type=float, help='Override default value for quadcol_bin reward')
     p.add_argument('--quads_settle_reward', default=None, type=float, help='Override default value for quadsettle reward')
     p.add_argument('--quads_dist_between_goals', default=0.3, type=float, help='Under circular configuration scenarios, it should be the radius of the circle of goals')
-    p.add_argument('--quads_mode', default='sanity_check', type=str, choices=['circular_config', 'same_goal', 'sanity_check'], help='Choose which scanerio to run')
+    p.add_argument('--quads_mode', default='sanity_check', type=str, choices=['circular_config', 'same_goal', 'sanity_check', 'lissajous3D'], help='Choose which scanerio to run')
     p.add_argument('--extend_obs', default=False, type=str2bool, help='Drones receive relative pos and relative vel info from all other drones')
     p.add_argument('--quads_use_numba', default=False, type=str2bool, help='Whether to use numba for jit or not')
     p.add_argument('--quads_settle', default=False, type=str2bool, help='Use velocity penalty and equal distance rewards when drones are within a certain radius of the goal')
