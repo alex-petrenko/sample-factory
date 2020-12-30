bc8d1cc5b9733566d7919ea5979c8871abc5275d
Aleksei Petrenko <petrenko@usc.edu>
2020-12-30 19:54:42 +0300

    Gridsearch collision reward

--- runner/runs/quad_multi_static_goal_3d_sphere.py
+++ runner/runs/quad_multi_static_goal_3d_sphere.py
@@ -2,16 +2,16 @@ from runner.run_description import RunDescription, Experiment, ParamGrid
 
 _params = ParamGrid([
     ('seed', [1111, 3333]),
-    ('policy_init_gain', [1.0, 0.5, 0.3, 0.1]),
+    ('quads_collision_reward', [0.0, 1.0, 2.0, 5.0]),
 ])
 
 _experiment = Experiment(
     '8_static_goal_sphere',
-    'python -m run_algorithm --env=quadrotor_multi --train_for_env_steps=2000000000 --algo=APPO --use_rnn=False --num_workers=36 --num_envs_per_worker=4 --learning_rate=0.0001 --ppo_clip_value=5.0 --recurrence=1 --nonlinearity=tanh --actor_critic_share_weights=False --policy_initialization=xavier_uniform --adaptive_stddev=False --hidden_size=256 --with_vtrace=False --max_policy_lag=100000000 --gae_lambda=1.00 --max_grad_norm=5.0 --exploration_loss_coeff=0.0 --rollout=128 --batch_size=1024 --extend_obs=True --quads_use_numba=True --quads_num_agents=8 --quads_episode_duration=15.0 --quads_mode=static_goal --quads_dist_between_goals=0.25 --quads_collision_reward=2.0 --encoder_custom=quad_multi_encoder_deepset --quads_goal_dimension=3D --with_pbt=False',
+    'python -m run_algorithm --env=quadrotor_multi --train_for_env_steps=2000000000 --algo=APPO --use_rnn=False --num_workers=36 --num_envs_per_worker=4 --learning_rate=0.0001 --ppo_clip_value=5.0 --recurrence=1 --nonlinearity=tanh --actor_critic_share_weights=False --policy_initialization=xavier_uniform --adaptive_stddev=False --hidden_size=256 --with_vtrace=False --max_policy_lag=100000000 --gae_lambda=1.00 --max_grad_norm=5.0 --exploration_loss_coeff=0.0 --rollout=128 --batch_size=1024 --extend_obs=True --quads_use_numba=True --quads_num_agents=8 --quads_episode_duration=15.0 --quads_mode=static_goal --quads_dist_between_goals=0.25 --quads_collision_reward=1.0 --encoder_custom=quad_multi_encoder_deepset --quads_goal_dimension=3D --with_pbt=False',
     _params.generate_params(randomize=False),
 )
 
-RUN_DESCRIPTION = RunDescription('quads_multi_static_goal_sphere_8_v112', experiments=[_experiment])
+RUN_DESCRIPTION = RunDescription('quads_multi_static_goal_sphere_8_col_rew_v112', experiments=[_experiment])
 
 # On Brain server, when you use num_workers = 72, if the system reports: Resource temporarily unavailable,
 # then, try to use two commands below
