213f479ae5664b089a2fb114c6573fe2f942e2b2
Aleksei Petrenko <petrenko@usc.edu>
2021-01-20 15:23:11 -0800

    Benchmarked RNN layers version, added experiments. Update to: Version V113

--- runner/runs/voxel_envs.py
+++ runner/runs/voxel_envs.py
@@ -1,14 +1,16 @@
 from runner.run_description import RunDescription, Experiment, ParamGrid
 
 _params = ParamGrid([
-    ('env', ['voxel_env_rearrange', 'voxel_env_collect', 'voxel_env_obstaclesEasy', 'voxel_env_hexExplore']),
+    # ('env', ['voxel_env_rearrange', 'voxel_env_collect', 'voxel_env_obstaclesEasy', 'voxel_env_hexExplore']),
+    ('env', ['voxel_env_collect']),
     ('voxel_num_simulation_threads', [1]),
+    ('rnn_num_layers', [1, 2, 3, 4]),
 ])
 
 _experiment = Experiment(
     'voxel_env_pbt',
-    'python -m algorithms.appo.train_appo --env=voxel_env_empty --train_for_seconds=360000000 --algo=APPO --gamma=0.997 --use_rnn=True --num_workers=12 --num_envs_per_worker=2 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --actor_worker_gpus 0 --num_policies=1 --with_pbt=False --max_grad_norm=0.0 --exploration_loss=symmetric_kl --exploration_loss_coeff=0.001 --voxel_num_envs_per_instance=32 --voxel_num_agents_per_env=1 --voxel_num_simulation_threads=1 --voxel_use_vulkan=True --policy_workers_per_policy=2 --learner_main_loop_num_cores=4',
+    'python -m algorithms.appo.train_appo --train_for_seconds=360000000 --algo=APPO --gamma=0.997 --use_rnn=True --num_workers=12 --num_envs_per_worker=2 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --actor_worker_gpus 0 --num_policies=1 --with_pbt=False --max_grad_norm=0.0 --exploration_loss=symmetric_kl --exploration_loss_coeff=0.001 --voxel_num_envs_per_instance=32 --voxel_num_agents_per_env=1 --voxel_num_simulation_threads=1 --voxel_use_vulkan=True --policy_workers_per_policy=2 --learner_main_loop_num_cores=4',
     _params.generate_params(randomize=False),
 )
 
-RUN_DESCRIPTION = RunDescription('voxel_env_v112_env_v46', experiments=[_experiment])
+RUN_DESCRIPTION = RunDescription('voxel_env_v113_env_v46_rnn', experiments=[_experiment])
