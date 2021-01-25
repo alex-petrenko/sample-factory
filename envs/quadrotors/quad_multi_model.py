3d6c556ae86a447952710cf3ff09d5893888a242
Aleksei Petrenko <petrenko@usc.edu>
2021-01-24 17:02:48 -0800

    Incomplete attention encoder for quadrotors

--- envs/quadrotors/quad_multi_model.py
+++ envs/quadrotors/quad_multi_model.py
@@ -6,10 +6,97 @@ from algorithms.appo.model_utils import nonlinearity, EncoderBase, \
 from algorithms.utils.pytorch_utils import calc_num_elements
 
 
+class QuadNeighborhoodEncoder(nn.Module):
+    def __init__(self, cfg, self_obs_dim, neighbor_obs_dim, neighbor_hidden_size, num_use_neighbor_obs):
+        super().__init__()
+        self.cfg = cfg
+        self.self_obs_dim = self_obs_dim
+        self.neighbor_obs_dim = neighbor_obs_dim
+        self.neighbor_hidden_size = neighbor_hidden_size
+        self.num_use_neighbor_obs = num_use_neighbor_obs
+
+
+class QuadNeighborhoodEncoderDeepsets(QuadNeighborhoodEncoder):
+    def __init__(self, cfg, neighbor_obs_dim, neighbor_hidden_size, use_spectral_norm, self_obs_dim, num_use_neighbor_obs):
+        super().__init__(cfg, self_obs_dim, neighbor_obs_dim, neighbor_hidden_size, num_use_neighbor_obs)
+
+        self.embedding_mlp = nn.Sequential(
+            fc_layer(neighbor_obs_dim, neighbor_hidden_size, spec_norm=use_spectral_norm),
+            nonlinearity(cfg),
+            fc_layer(neighbor_hidden_size, neighbor_hidden_size, spec_norm=use_spectral_norm),
+            nonlinearity(cfg)
+        )
+
+    def forward(self, self_obs, obs, all_neighbor_obs_size, batch_size):
+        obs_neighbors = obs[:, self.self_obs_dim:self.self_obs_dim + all_neighbor_obs_size]
+        obs_neighbors = obs_neighbors.reshape(-1, self.neighbor_obs_dim)
+        neighbor_embeds = self.embedding_mlp(obs_neighbors)
+        neighbor_embeds = neighbor_embeds.reshape(batch_size, -1, self.neighbor_hidden_size)
+        mean_embed = torch.mean(neighbor_embeds, dim=1)
+        return mean_embed
+
+
+
+class QuadNeighborhoodEncoderAttention(QuadNeighborhoodEncoder):
+    def __init__(self, cfg, neighbor_obs_dim, neighbor_hidden_size, use_spectral_norm, self_obs_dim, num_use_neighbor_obs):
+        super().__init__(cfg, self_obs_dim, neighbor_obs_dim, neighbor_hidden_size, num_use_neighbor_obs)
+
+        self.self_obs_dim = self_obs_dim
+
+        self.embedding_mlp = nn.Sequential(
+            fc_layer(self_obs_dim + neighbor_obs_dim, neighbor_hidden_size, spec_norm=use_spectral_norm),
+            nonlinearity(cfg),
+            fc_layer(neighbor_hidden_size, neighbor_hidden_size, spec_norm=use_spectral_norm),
+            nonlinearity(cfg)
+        )
+
+        self.neighbor_value_mlp = nn.Sequential(
+            fc_layer(neighbor_hidden_size, neighbor_hidden_size, spec_norm=use_spectral_norm),
+            nonlinearity(cfg),
+            fc_layer(neighbor_hidden_size, neighbor_hidden_size, spec_norm=use_spectral_norm),
+            nonlinearity(cfg),
+        )
+
+        self.attention_mlp = nn.Sequential(
+            fc_layer(neighbor_hidden_size * 2, neighbor_hidden_size, spec_norm=use_spectral_norm),  # neighbor_hidden_size * 2 because we concat e_i and e_m
+            nonlinearity(cfg),
+            fc_layer(neighbor_hidden_size, neighbor_hidden_size, spec_norm=use_spectral_norm),
+            nonlinearity(cfg),
+        )
+
+    def forward(self, self_obs, obs, all_neighbor_obs_size, batch_size):
+        obs_neighbors = obs[:, self.self_obs_dim:self.self_obs_dim + all_neighbor_obs_size]
+        obs_neighbors = obs_neighbors.reshape(-1, self.neighbor_obs_dim)
+
+        # concatenate self observation with neighbor observation
+
+        self_obs_repeat = self_obs.repeat(self.num_use_neighbor_obs, 1)
+        mlp_input = torch.cat((self_obs_repeat, obs_neighbors), dim=1)
+        neighbor_embeddings = self.embedding_mlp(mlp_input)  # e_i in the paper https://arxiv.org/pdf/1809.08835.pdf
+
+        neighbor_values = self.neighbor_value_mlp(neighbor_embeddings)  # h_i in the paper
+
+        neighbor_embeddings_mean_input = neighbor_embeddings.reshape(batch_size, -1, self.neighbor_hidden_size)
+        neighbor_embeddings_mean = torch.mean(neighbor_embeddings_mean_input, dim=1)  # e_m in the paper
+        neighbor_embeddings_mean_repeat = neighbor_embeddings_mean.repeat(self.num_use_neighbor_obs, 1)
+
+        attention_mlp_input = torch.cat((neighbor_embeddings, neighbor_embeddings_mean_repeat), dim=1)
+        attention_weights = self.attention_mlp(attention_mlp_input)  # alpha_i in the paper
+
+        attention_weights_softmax = torch.nn.functional.softmax(attention_weights, dim=1)
+
+        final_neighborhood_embedding = attention_weights_softmax * neighbor_values
+        final_neighborhood_embedding = torch.sum(final_neighborhood_embedding, dim=1)
+
+        return final_neighborhood_embedding
+
+
 class QuadMultiMeanEncoder(EncoderBase):
     # Mean embedding encoder based on the DeepRL for Swarms Paper
     def __init__(self, cfg, obs_space, timing, self_obs_dim=18, neighbor_obs_dim=6, neighbor_hidden_size=32, obstacle_obs_dim=6, obstacle_hidden_size=32):
         super().__init__(cfg, timing)
+        self.neighbor_encoder_type = 'attention'  # TODO: config
+
         self.self_obs_dim = self_obs_dim
         self.neighbor_obs_type = cfg.neighbor_obs_type
         self.neighbor_hidden_size = neighbor_hidden_size
@@ -44,13 +131,14 @@ class QuadMultiMeanEncoder(EncoderBase):
         # encode the neighboring drone's observations
         neighbor_encoder_out_size = 0
         if self.num_use_neighbor_obs > 0:
-            self.neighbor_encoder = nn.Sequential(
-                fc_layer(self.neighbor_obs_dim, self.neighbor_hidden_size, spec_norm=self.use_spectral_norm),
-                nonlinearity(cfg),
-                fc_layer(self.neighbor_hidden_size, self.neighbor_hidden_size, spec_norm=self.use_spectral_norm),
-                nonlinearity(cfg)
-            )
-            neighbor_encoder_out_size = calc_num_elements(self.neighbor_encoder, (self.neighbor_obs_dim,))
+            if self.neighbor_encoder_type == 'deepsets':
+                self.neighbor_encoder = QuadNeighborhoodEncoderDeepsets(cfg, self.neighbor_obs_dim, self.neighbor_hidden_size, self.use_spectral_norm, self.self_obs_dim, self.num_use_neighbor_obs)
+            elif self.neighbor_encoder_type == 'attention':
+                self.neighbor_encoder = QuadNeighborhoodEncoderAttention(cfg, self.neighbor_obs_dim, self.neighbor_hidden_size, self.use_spectral_norm, self.self_obs_dim, self.num_use_neighbor_obs)
+            else:
+                raise NotImplementedError()
+
+            neighbor_encoder_out_size = self.neighbor_hidden_size
 
         # encode the obstacle observations
         obstacle_encoder_out_size = 0
@@ -84,12 +172,8 @@ class QuadMultiMeanEncoder(EncoderBase):
         # relative xyz and vxyz for the entire minibatch (batch dimension is batch_size * num_neighbors)
         all_neighbor_obs_size = self.neighbor_obs_dim * self.num_use_neighbor_obs
         if self.num_use_neighbor_obs > 0:
-            obs_neighbors = obs[:, self.self_obs_dim:self.self_obs_dim + all_neighbor_obs_size]
-            obs_neighbors = obs_neighbors.reshape(-1, self.neighbor_obs_dim)
-            neighbor_embeds = self.neighbor_encoder(obs_neighbors)
-            neighbor_embeds = neighbor_embeds.reshape(batch_size, -1, self.neighbor_hidden_size)
-            mean_embed = torch.mean(neighbor_embeds, dim=1)
-            embeddings = torch.cat((embeddings, mean_embed), dim=1)
+            neighborhood_embedding = self.neighbor_encoder(obs_self, obs, all_neighbor_obs_size, batch_size)
+            embeddings = torch.cat((embeddings, neighborhood_embedding), dim=1)
 
         if self.obstacle_mode != 'no_obstacles':
             obs_obstacles = obs[:, self.self_obs_dim + all_neighbor_obs_size:]
--- runner/runs/voxel_envs.py
+++ runner/runs/voxel_envs.py
@@ -8,7 +8,7 @@ _params = ParamGrid([
 
 _experiment = Experiment(
     'voxel_env_pbt',
-    'python -m algorithms.appo.train_appo --train_for_seconds=360000000 --algo=APPO --gamma=0.997 --use_rnn=True --num_workers=12 --num_envs_per_worker=2 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --actor_worker_gpus 0 --num_policies=1 --with_pbt=False --max_grad_norm=0.0 --exploration_loss=symmetric_kl --exploration_loss_coeff=0.001 --voxel_num_envs_per_instance=32 --voxel_num_agents_per_env=1 --voxel_num_simulation_threads=1 --voxel_use_vulkan=True --policy_workers_per_policy=2 --learner_main_loop_num_cores=4',
+    'python -m algorithms.appo.train_appo --train_for_seconds=360000000 --algo=APPO --gamma=0.997 --use_rnn=True --num_workers=12 --num_envs_per_worker=2 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --actor_worker_gpus 0 --num_policies=1 --with_pbt=False --max_grad_norm=0.0 --exploration_loss=symmetric_kl --exploration_loss_coeff=0.001 --voxel_num_envs_per_instance=36 --voxel_num_agents_per_env=1 --voxel_num_simulation_threads=1 --voxel_use_vulkan=True --policy_workers_per_policy=2 --learner_main_loop_num_cores=4',
     _params.generate_params(randomize=False),
 )
 
