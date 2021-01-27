b36b9e34ec89d067a427f1177f14aae36cad4976
Aleksei Petrenko <petrenko@usc.edu>
2021-01-27 01:29:38 -0800

    Experiments

--- runner/runs/voxel_multi_agent.py
+++ runner/runs/voxel_multi_agent.py
@@ -5,7 +5,7 @@ _params = ParamGrid([
     ('use_cpc', ['True', 'False'])
 ])
 
-_cli = 'python -m algorithms.appo.train_appo --train_for_seconds=360000000 --train_for_env_steps=1000000000 --algo=APPO --gamma=0.997 --use_rnn=True --rnn_num_layers=2 --num_workers=12 --num_envs_per_worker=2 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --actor_worker_gpus 0 --num_policies=1 --with_pbt=False --max_grad_norm=0.0 --exploration_loss=symmetric_kl --exploration_loss_coeff=0.001 --voxel_num_simulation_threads=1 --voxel_use_vulkan=True --policy_workers_per_policy=2 --learner_main_loop_num_cores=1'
+_cli = 'python -m algorithms.appo.train_appo --train_for_seconds=360000000 --train_for_env_steps=1000000000 --algo=APPO --gamma=0.997 --use_rnn=True --rnn_num_layers=2 --num_workers=12 --num_envs_per_worker=2 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --actor_worker_gpus 0 --num_policies=1 --with_pbt=False --max_grad_norm=0.0 --exploration_loss=symmetric_kl --exploration_loss_coeff=0.001 --voxel_num_simulation_threads=1 --voxel_use_vulkan=True --policy_workers_per_policy=2 --learner_main_loop_num_cores=1 --reward_clip=30'
 
 _experiment_2agents = Experiment(
     'voxel_env_2ag',
