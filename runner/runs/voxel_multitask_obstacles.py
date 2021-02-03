51cf0515e6eb1fe0377438c2a6cdd206d36c5288
Aleksei Petrenko <petrenko@usc.edu>
2021-02-03 02:57:09 -0800

    Minor change in experiment

--- runner/runs/voxel_multitask_obstacles.py
+++ runner/runs/voxel_multitask_obstacles.py
@@ -6,7 +6,7 @@ _params = ParamGrid([
     ('seed', [11111, 22222, 33333, 44444]),
 ])
 
-_cli = 'python -m algorithms.appo.train_appo --train_for_seconds=360000000 --train_for_env_steps=20000000000 --algo=APPO --gamma=0.997 --use_rnn=True --rnn_num_layers=2 --num_workers=12 --num_envs_per_worker=2 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --actor_worker_gpus 0 --num_policies=1 --with_pbt=False --max_grad_norm=0.0 --exploration_loss=symmetric_kl --exploration_loss_coeff=0.001 --voxel_num_simulation_threads=1 --voxel_use_vulkan=True --policy_workers_per_policy=2 --learner_main_loop_num_cores=1 --reward_clip=30'
+_cli = 'python -m algorithms.appo.train_appo --train_for_seconds=360000000 --train_for_env_steps=20000000000 --algo=APPO --gamma=0.997 --use_rnn=True --rnn_num_layers=2 --num_workers=12 --num_envs_per_worker=2 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --actor_worker_gpus 0 --num_policies=1 --with_pbt=False --max_grad_norm=0.0 --exploration_loss=symmetric_kl --exploration_loss_coeff=0.001 --voxel_num_simulation_threads=1 --voxel_use_vulkan=True --policy_workers_per_policy=2 --learner_main_loop_num_cores=1 --reward_clip=30 --pbt_mix_policies_in_one_env=False'
 
 EXPERIMENT_1AGENT = Experiment(
     'voxel_env_multitask_obs',
