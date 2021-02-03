c09b86bb959a79916b5c91f64feb64db65481090
Aleksei Petrenko <petrenko@usc.edu>
2021-02-03 00:36:04 -0800

    Benchmarking scripts for the ICML paper

--- envs/atari/atari_params.py
+++ envs/atari/atari_params.py
@@ -8,7 +8,7 @@ def atari_override_defaults(env, parser):
         gamma=0.99,
         reward_clip=1.0,  # same as APE-X paper
         env_frameskip=4,
-        env_framestack=4,
+        env_framestack=1,  # be careful about this, only works with RNNs
         exploration_loss_coeff=0.01,
     )
 
