5293c39ab15fb12d8cbaeb31ce736d9e75cd351c
Zhehui Huang <zhehuihu@usc.edu>
2021-02-05 16:40:14 -0800

    Modify runner script for curriculum

--- runner/runs/quad_multi_mixed_curriculum.py
+++ runner/runs/quad_multi_mixed_curriculum.py
@@ -2,11 +2,11 @@ from runner.run_description import RunDescription, Experiment, ParamGrid
 
 _params = ParamGrid([
     ('seed', [0000, 1111, 2222, 3333]),
-    ('quads_curriculum_mode', 'none', 'epsilon_greedy', 'UCB1')
+    ('quads_curriculum_mode', ['none', 'epsilon_greedy', 'UCB1'])
 ])
 
 _experiment = Experiment(
-    'local_map-col_mode-8_mixed',
+    'use_curriculum-col_mode-8_mixed',
     'python -m run_algorithm --env=quadrotor_multi --train_for_env_steps=5000000000 --algo=APPO --use_rnn=False --num_workers=36 --num_envs_per_worker=4 --learning_rate=0.0001 --ppo_clip_value=5.0 --recurrence=1 --nonlinearity=tanh --actor_critic_share_weights=False --policy_initialization=xavier_uniform --adaptive_stddev=False --hidden_size=256 --quads_neighbor_hidden_size=256 --with_vtrace=False --max_policy_lag=100000000 --gae_lambda=1.00 --max_grad_norm=5.0 --exploration_loss_coeff=0.0 --rollout=128 --batch_size=1024 --quads_use_numba=True --quads_num_agents=8 --quads_episode_duration=15.0 --quads_mode=mix --quads_formation_size=0.0 --encoder_custom=quad_multi_encoder_deepset --with_pbt=False --quads_collision_reward=5.0 --neighbor_obs_type=pos_vel',
     _params.generate_params(randomize=False),
 )
