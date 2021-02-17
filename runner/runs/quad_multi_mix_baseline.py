4a4dc7423d5a82d9db3fc3810e8e9ea6f1ddb584
Zhehui Huang <30305127+Zhehui-Huang@users.noreply.github.com>
2021-02-17 00:51:12 -0800

    Update the baseline runner script, add local info (#98)

--- runner/runs/quad_multi_mix_baseline.py
+++ runner/runs/quad_multi_mix_baseline.py
@@ -6,7 +6,7 @@ _params = ParamGrid([
 
 _experiment = Experiment(
     'quad_mix_baseline-16_mixed',
-    'python -m run_algorithm --env=quadrotor_multi --train_for_env_steps=5000000000 --algo=APPO --use_rnn=False '
+    'python -m run_algorithm --env=quadrotor_multi --train_for_env_steps=2000000000 --algo=APPO --use_rnn=False '
     '--num_workers=72 --num_envs_per_worker=4 --learning_rate=0.0001 --ppo_clip_value=5.0 --recurrence=1 '
     '--nonlinearity=tanh --actor_critic_share_weights=False --policy_initialization=xavier_uniform '
     '--adaptive_stddev=False --with_vtrace=False --max_policy_lag=100000000 --hidden_size=256 '
@@ -17,7 +17,8 @@ _experiment = Experiment(
     '--quads_settle_reward=0.1 --quads_collision_hitbox_radius=2.0 --quads_collision_falloff_radius=4.0 '
     '--quads_collision_smooth_max_penalty=10.0 --quads_collision_vel_penalty_mode=quadratic '
     '--quads_collision_smooth_vel_coeff=2.0 --quads_collision_vel_penalty_radius=6.0 '
-    '--quads_collision_smooth_vel_max_penalty=10.0 --quads_num_agents=16',
+    '--quads_collision_smooth_vel_max_penalty=10.0 --quads_local_obs=8 --quads_local_metric=dist '
+    '--quads_local_coeff=1.0 --quads_num_agents=16',
     _params.generate_params(randomize=False),
 )
 
