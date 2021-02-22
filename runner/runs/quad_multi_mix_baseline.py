90f3708a578588c8f231a3c7c880826219c3d45d
Aleksei Petrenko <petrenko@usc.edu>
2021-02-22 03:53:23 -0800

    Changed the number of steps in experiments

--- runner/runs/quad_multi_mix_baseline.py
+++ runner/runs/quad_multi_mix_baseline.py
@@ -7,7 +7,7 @@ _params = ParamGrid([
 ])
 
 QUAD_BASELINE_CLI = (
-    'python -m run_algorithm --env=quadrotor_multi --train_for_env_steps=2000000000 --algo=APPO --use_rnn=False '
+    'python -m run_algorithm --env=quadrotor_multi --train_for_env_steps=1000000000 --algo=APPO --use_rnn=False '
     '--num_workers=36 --num_envs_per_worker=4 --learning_rate=0.0001 --ppo_clip_value=5.0 --recurrence=1 '
     '--nonlinearity=tanh --actor_critic_share_weights=False --policy_initialization=xavier_uniform '
     '--adaptive_stddev=False --with_vtrace=False --max_policy_lag=100000000 --hidden_size=256 '
