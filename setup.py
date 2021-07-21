f4856123f0498c5d2d6e3d482f117acc07988001
Aleksei Petrenko <petrenko@usc.edu>
2021-07-21 03:31:17 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     description='High throughput asynchronous reinforcement learning framework',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.120.0',
+    version='1.120.1',
     url='https://github.com/alex-petrenko/sample-factory',
     author='Aleksei Petrenko',
     license='MIT',
