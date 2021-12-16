9acd698b375e69da1666b2df1c3929ad9065b743
Aleksei Petrenko <petrenko@usc.edu>
2021-12-15 18:07:00 -0800

    Minor changes

--- sample_factory/runner/runs/doom_battle_battle2_appo.py
+++ sample_factory/runner/runs/doom_battle_battle2_appo.py
@@ -1,9 +1,14 @@
+"""
+Run this on Slurm with the following command (assuming you are in sample-factory repo root):
+python -m sample_factory.runner.run --run=sample_factory.runner.runs.doom_battle_battle2_appo --runner=slurm --slurm_workdir=./slurm_doom_battle_battle2 --experiment_suffix=slurm --pause_between=1 --slurm_gpus_per_job=1 --slurm_cpus_per_gpu=16 --slurm_sbatch_template=./sample_factory/runner/slurm/sbatch_template.sh --slurm_print_only=False
+"""
+
 from sample_factory.runner.run_description import RunDescription, Experiment, ParamGrid
 from sample_factory.runner.runs.run_utils import seeds
 
 _params = ParamGrid([
-    ('seed', seeds(4)),
     ('env', ['doom_battle', 'doom_battle2']),
+    ('seed', seeds(4)),
 ])
 
 _experiments = [
