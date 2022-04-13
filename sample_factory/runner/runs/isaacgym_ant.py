27cdccfa42f5c8f3978e8b725624b0ac0d770d15
Aleksei Petrenko <petrenko@usc.edu>
2022-04-13 09:04:07 -0700

    RNN async experiment

--- sample_factory/runner/runs/isaacgym_ant.py
+++ sample_factory/runner/runs/isaacgym_ant.py
@@ -2,19 +2,20 @@ from sample_factory.runner.run_description import RunDescription, Experiment, Pa
 
 _params = ParamGrid([
     ('seed', [1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888]),
-    ('serial_mode', [True, False]),
+    ('serial_mode', [False]),
     ('async_rl', [True, False]),
 ])
 
-_version = 35
+_version = 36
 _vstr = f'v{_version:03d}'
 _cli = f'python -m sample_factory_examples.train_isaacgym ' \
        f'--algo=APPO --env=isaacgym_ant --actor_worker_gpus 0 --train_for_env_steps=100000000 --env_agents=4096 ' \
-       f'--batch_size=32768 --env_headless=True --use_rnn=False --with_vtrace=False --recurrence=1 --with_wandb=True ' \
+       f'--batch_size=32768 --env_headless=True --with_vtrace=False --use_rnn=False --recurrence=1 --with_wandb=True ' \
        f'--wandb_group=isaacgym_ant_sf2 --wandb_tags {_vstr}'
 
 _experiments = [
     Experiment(f'ant_{_vstr}_async', _cli, _params.generate_params(False)),
+    Experiment(f'ant_{_vstr}_async_rnn', _cli + ' --use_rnn=True --recurrence=16', _params.generate_params(False)),
 ]
 
 RUN_DESCRIPTION = RunDescription(f'ant_{_vstr}', experiments=_experiments)
