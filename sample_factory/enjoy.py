1d307b3dc8c7ca03850c56d85813b1a0ff399d51
Aleksei Petrenko <petrenko@usc.edu>
2022-06-22 20:08:13 -0700

    Fixed issues with action dimensions when we only have one action and one env. Version V57

--- sample_factory/enjoy.py
+++ sample_factory/enjoy.py
@@ -8,7 +8,7 @@ import torch
 from sample_factory.algo.learning.learner import Learner
 from sample_factory.algo.sampling.batched_sampling import preprocess_actions
 from sample_factory.algo.utils.env_info import extract_env_info
-from sample_factory.algo.utils.make_env import make_env_func_batched
+from sample_factory.algo.utils.make_env import make_env_func_batched, SequentialVectorizeWrapper
 from sample_factory.algo.utils.misc import ExperimentStatus
 from sample_factory.model.model import create_actor_critic
 from sample_factory.model.model_utils import get_hidden_size
@@ -31,13 +31,10 @@ def enjoy(cfg, max_num_frames=1e9):
     cfg.num_envs = 1
 
     env = make_env_func_batched(cfg, env_config=AttrDict(worker_index=0, vector_index=0, env_id=0))
+    if env.num_agents == 1:  # TODO: fix type warnings and missing attribute warnings
+        env = SequentialVectorizeWrapper([env])
     # env.seed(0)  # TODO: make a parameter for this?
     env_info = extract_env_info(env, cfg)
-
-    # TODO: use the same code as in the sampler
-    is_multiagent = is_multiagent_env(env)
-    if not is_multiagent:
-        env = MultiAgentWrapper(env)
 
     if hasattr(env.unwrapped, 'reset_on_init'):
         # reset call ruins the demo recording for VizDoom
