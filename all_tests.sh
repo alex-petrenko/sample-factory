564284ce3c7fdbfafe36180d3d41466497a98ba9
wmFrank <mwang283@usc.edu>
2022-06-26 14:22:57 -0700

    seperate test & coverage

--- all_tests.sh
+++ all_tests.sh
@@ -6,10 +6,10 @@ STATUS_ALL_TESTS=$?
 python -m unittest sample_factory_examples.tests.test_example
 STATUS_TEST_EXAMPLE=$?
 
-#python -m unittest sample_factory_examples.tests.test_example_multi
-#STATUS_TEST_EXAMPLE_MULTI=$?
+python -m unittest sample_factory_examples.tests.test_example_multi
+STATUS_TEST_EXAMPLE_MULTI=$?
 
 echo "Your terminal might be unresponsive (caused by Doom envs manipulating the console). Type reset in your terminal and press Enter to get it back to normal"
 echo "Unit tests status (0 = Success): $STATUS_ALL_TESTS"
 echo "Custom env status (0 = Success): $STATUS_TEST_EXAMPLE"
-#echo "Custom multi env status (0 = Success): $STATUS_TEST_EXAMPLE_MULTI"
+echo "Custom multi env status (0 = Success): $STATUS_TEST_EXAMPLE_MULTI"
---
+++ sample_factory_examples/tests/test_error.py
@@ -0,0 +1,6 @@
+import unittest
+
+class TestCI(TestCase):
+    def test_ci(self):
+        x = 10
+        self.assertEqual(status, 20)
