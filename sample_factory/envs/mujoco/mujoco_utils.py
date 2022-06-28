8256b3e32c16c2dff3dc8c47c4a330885207b83f
Andrew Zhang <31525996+andrewzhang505@users.noreply.github.com>
2022-06-27 19:04:49 -0700

    Sf2 mujoco (#143)
    * SF2 changes to train mujoco
    
    * Added enjoy and runner
    
    * PR changes
    
    * mujoco runner seeds
    
    * Mujoco tests
    
    * fixed typo

--- sample_factory/envs/mujoco/mujoco_utils.py
+++ sample_factory/envs/mujoco/mujoco_utils.py
@@ -8,9 +8,17 @@ class MujocoSpec:
 
 
 MUJOCO_ENVS = [
-    MujocoSpec('mujoco_hopper', 'Hopper-v2'),
-    MujocoSpec('mujoco_halfcheetah', 'HalfCheetah-v2'),
-    MujocoSpec('mujoco_humanoid', 'Humanoid-v2'),
+    MujocoSpec('mujoco_hopper', 'Hopper-v4'),
+    MujocoSpec('mujoco_halfcheetah', 'HalfCheetah-v4'),
+    MujocoSpec('mujoco_humanoid', 'Humanoid-v4'),
+    MujocoSpec('mujoco_ant', 'Ant-v4'),
+    MujocoSpec('mujoco_standup', 'HumanoidStandup-v4'),
+    MujocoSpec('mujoco_doublependulum', 'InvertedDoublePendulum-v4'),
+    MujocoSpec('mujoco_pendulum', 'InvertedPendulum-v4'),
+    MujocoSpec('mujoco_reacher', 'Reacher-v4'),
+    MujocoSpec('mujoco_walker', 'Walker2d-v4'),
+    MujocoSpec('mujoco_pusher', 'Pusher-v4'),
+    MujocoSpec('mujoco_swimmer', 'Swimmer-v4'),
 ]
 
 
@@ -25,3 +33,4 @@ def make_mujoco_env(env_name, cfg, **kwargs):
     mujoco_spec = mujoco_env_by_name(env_name)
     env = gym.make(mujoco_spec.env_id)
     return env
+
