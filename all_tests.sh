4627041a91162c9c880ff4a4024c98be6cfe5ba3
wmFrank <mwang283@usc.edu>
2022-06-28 11:08:29 -0700

    add mujoco test cmd

--- all_tests.sh
+++ all_tests.sh
@@ -3,5 +3,9 @@
 python -m unittest
 STATUS_ALL_TESTS=$?
 
+python -m unittest sample_factory_examples.mujoco_examples.test_mujoco
+STATUS_TEST_MUJOCO=$?
+
 echo "Your terminal might be unresponsive (caused by Doom envs manipulating the console). Type reset in your terminal and press Enter to get it back to normal"
 echo "Unit tests status (0 = Success): $STATUS_ALL_TESTS"
+echo "Mujoco status (0 = Success): $STATUS_TEST_MUJOCO"
