0b703d41ed62caacc798b1c78cf1efab2e2babf6
Andrew Zhang <gnahzwerdna@gmail.com>
2022-06-29 23:36:47 +0000

    More info on gae test

--- sample_factory_examples/mujoco_examples/test_mujoco.py
+++ sample_factory_examples/mujoco_examples/test_mujoco.py
@@ -5,7 +5,9 @@ from sample_factory.algo.utils.misc import ExperimentStatus, EPS
 from sample_factory.envs.mujoco.mujoco_utils import mujoco_available
 from sample_factory.train import run_rl
 from sample_factory.utils.utils import log
-from sample_factory_examples.train_custom_env_custom_model import register_custom_components, custom_parse_args
+from sample_factory_examples.mujoco_examples.train_mujoco import register_mujoco_components
+from sample_factory.cfg.arguments import parse_args
+
 
 
 @unittest.skipUnless(mujoco_available(), 'Please install Mujoco to run a full test suite')
@@ -20,23 +22,23 @@ class TestMujoco(TestCase):
     def _run_test_env(
             self, env: str = 'mujoco_ant', num_workers: int = 8, train_steps: int = 100,
             expected_reward_at_least: float = -EPS, batched_sampling: bool = False,
-            serial_mode: bool = False, async_rl: bool = True,
+            serial_mode: bool = False, async_rl: bool = True, batch_size: int = 64, 
     ):
         log.debug(f'Testing with parameters {locals()}...')
 
         experiment_name = 'test_' + env
 
-        register_custom_components()
+        register_mujoco_components()
 
         # test training for a few thousand frames
-        cfg = custom_parse_args(argv=['--algo=APPO', f'--env={env}', f'--experiment={experiment_name}'])
+        cfg = parse_args(argv=['--algo=APPO', f'--env={env}', f'--experiment={experiment_name}'])
         cfg.serial_mode = serial_mode
         cfg.async_rl = async_rl
         cfg.batched_sampling = batched_sampling
         cfg.num_workers = num_workers
         cfg.num_envs_per_worker = 2
         cfg.train_for_env_steps = train_steps
-        cfg.batch_size = 64
+        cfg.batch_size = batch_size
         cfg.seed = 0
         cfg.device = 'cpu'
 
@@ -68,10 +70,10 @@ class TestMujoco(TestCase):
     @unittest.skip('broken tests not fixed yet')
     def test_fail_gae(self):
         """
-        Currently failing tests due to gae
+        Currently failing tests due to gae. Test only fails when batch size is small
         """
         env_list = ['mujoco_hopper', 'mujoco_reacher', 'mujoco_walker', 'mujoco_swimmer']
         for env in env_list:
             self._run_test_env(
-                env=env, num_workers=1, train_steps=100,
+                env=env, num_workers=1, train_steps=100, batch_size=64 # Setting batch size = 1024 makes it pass
             )
