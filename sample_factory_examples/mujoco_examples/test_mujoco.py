8ae8547b5ea00916f243ef2c15d72a02b8326fe9
Aleksei Petrenko <petrenko@usc.edu>
2022-06-28 19:03:46 -0700

    Small changes to mujoco test

--- sample_factory_examples/mujoco_examples/test_mujoco.py
+++ sample_factory_examples/mujoco_examples/test_mujoco.py
@@ -1,21 +1,14 @@
-import os
-import shutil
 import unittest
-from os.path import isdir
 from unittest import TestCase
 
 from sample_factory.algo.utils.misc import ExperimentStatus, EPS
-from sample_factory.enjoy import enjoy
+from sample_factory.envs.mujoco.mujoco_utils import mujoco_available
 from sample_factory.train import run_rl
+from sample_factory.utils.utils import log
 from sample_factory_examples.train_custom_env_custom_model import register_custom_components, custom_parse_args
-from sample_factory.utils.utils import experiment_dir, log
 
 
-@unittest.skipIf(
-    'SKIP_TESTS_THAT_REQUIRE_A_SEPARATE_PROCESS' in os.environ,
-    'this test should be executed in a separate process because of how PyTorch works: '
-    'https://github.com/pytorch/pytorch/issues/33248',
-)
+@unittest.skipUnless(mujoco_available(), 'Please install Mujoco to run a full test suite')
 class TestMujoco(TestCase):
     """
     This test does not work if other tests used PyTorch autograd before it.
@@ -25,7 +18,7 @@ class TestMujoco(TestCase):
     """
 
     def _run_test_env(
-            self, env: str = "mujoco_ant", num_workers: int = 8, train_steps: int = 100,
+            self, env: str = 'mujoco_ant', num_workers: int = 8, train_steps: int = 100,
             expected_reward_at_least: float = -EPS, batched_sampling: bool = False,
             serial_mode: bool = False, async_rl: bool = True,
     ):
@@ -43,6 +36,7 @@ class TestMujoco(TestCase):
         cfg.num_workers = num_workers
         cfg.num_envs_per_worker = 2
         cfg.train_for_env_steps = train_steps
+        cfg.batch_size = 64
         cfg.seed = 0
         cfg.device = 'cpu'
 
@@ -54,8 +48,9 @@ class TestMujoco(TestCase):
         Run the test env in various configurations just to make sure nothing crashes or throws exceptions.
         """
         # Currently fails on last 2 environments
-        env_list = ["mujoco_ant", "mujoco_halfcheetah", "mujoco_hopper", "mujoco_humanoid", "mujoco_reacher", "mujoco_walker", "mujoco_swimmer", "mujoco_pendulum", "mujoco_doublependulum"]
+        env_list = ['mujoco_ant', 'mujoco_halfcheetah', 'mujoco_hopper', 'mujoco_humanoid', 'mujoco_reacher',
+                    'mujoco_walker', 'mujoco_swimmer', 'mujoco_pendulum', 'mujoco_doublependulum']
         for env in env_list:
             self._run_test_env(
-                env=env, num_workers=1, train_steps=50,
+                env=env, num_workers=1, train_steps=100,
             )
