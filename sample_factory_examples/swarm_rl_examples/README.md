d623e8d1e1238db507fb772dcd9684bd9326256e
Andrew Zhang <gnahzwerdna@gmail.com>
2022-07-07 23:38:44 +0000

    Added single drone runner

--- sample_factory_examples/swarm_rl_examples/README.md
+++ sample_factory_examples/swarm_rl_examples/README.md
@@ -1,7 +1,7 @@
 ## Installing swarm-rl
 1. Clone https://github.com/Zhehui-Huang/quad-swarm-rl into your home directory
 2. Follow the instructions in the repo to create the conda environment
-3. Install the gym_art folder by adding    `export PATH=$PATH:~/quad_swarm_rl` to your .bashrc
+3. Install the gym_art folder by adding  `export PATH=$PATH:~/quad-swarm-rl` to your .bashrc
 
 ## Running an example
 ```
@@ -11,4 +11,9 @@ python -m sample_factory_examples.swarm_rl_examples.train_swarm_rl --env=quadrot
 Enjoy 
 ```
 python -m sample_factory_examples.swarm_rl_examples.enjoy_swarm_rl --algo=APPO --env=quadrotor_multi --replay_buffer_sample_prob=0 --continuous_actions_sample=False --quads_use_numba=False --experiment=swarm_rl
+```
+
+Using the runner with a single drone
+```
+python -m sample_factory.runner.run --run=swarm_rl.runs.single_drone --runner=processes --max_parallel=1 --pause_between=1 --experiments_per_gpu=1 --num_gpus=1
 ```
---
+++ sample_factory_examples/swarm_rl_examples/runs/single_drone.py
@@ -0,0 +1,21 @@
+from sample_factory.runner.run_description import RunDescription, Experiment, ParamGrid
+from sample_factory_examples.swarm_rl_examples.runs.quad_multi_mix_baseline import QUAD_BASELINE_CLI
+
+_params = ParamGrid([
+    ('seed', [0000])#, 1111, 2222, 3333]),
+])
+
+SMALL_MODEL_CLI = QUAD_BASELINE_CLI + (
+    ' --train_for_env_steps=10000000000 --hidden_size=16 --neighbor_obs_type=none --quads_local_obs=-1 '
+    '--quads_num_agents=1 --replay_buffer_sample_prob=0.0 --anneal_collision_steps=0 --save_milestones_sec=10000 '
+    '--quads_neighbor_encoder_type=no_encoder --with_wandb=True'
+)
+
+_experiment = Experiment(
+    'baseline',
+    SMALL_MODEL_CLI,
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription('quad_single_baseline', experiments=[_experiment])
+# python -m sample_factory.runner.run --run=sample_factory_examples.swarm_rl_examples.runs.single_drone --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=1
