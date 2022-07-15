b7267e6d5c9391c449b3ea8048ad9c8109128efa
Andrew Zhang <gnahzwerdna@gmail.com>
2022-07-15 19:42:26 +0000

    Gym version 0.25 breaks our mujoco install procedure

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires=[
         "numpy>=1.18.1<2.0",
         "torch>=1.6,<1.12",
-        "gym>=0.17.1,<1.0",
+        "gym>=0.17.1,<0.25",
         "pyglet",  # gym dependency
         "tensorboard>=1.15.0",
         "tensorboardx>=2.0",
