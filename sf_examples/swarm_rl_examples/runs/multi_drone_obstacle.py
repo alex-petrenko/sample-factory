1da4ae5b50d4d0dbca38398d7280ca2a5e6930e2
Andrew Zhang <gnahzwerdna@gmail.com>
2022-08-17 23:45:34 +0000

    Obstacle runner

--- sf_examples/swarm_rl_examples/runs/multi_drone_obstacle.py
+++ sf_examples/swarm_rl_examples/runs/multi_drone_obstacle.py
@@ -3,12 +3,12 @@ from sf_examples.swarm_rl_examples.runs.quad_multi_mix_baseline import QUAD_BASE
 
 _params = ParamGrid(
     [
-        ("seed", [0000]),
+        ("seed", [0000, 1111, 2222, 3333]),
     ]
 )
 
 SMALL_MODEL_CLI = QUAD_BASELINE_CLI + (
-    " --train_for_env_steps=10000000000 --num_workers=16 --num_envs_per_worker=8 "
+    " --train_for_env_steps=10000000000 --num_workers=36 --num_envs_per_worker=4 "
     "--quads_num_agents=8 --save_milestones_sec=10000 --async_rl=True --num_batches_to_accumulate=8 "
     "--serial_mode=False --batched_sampling=True --normalize_input=True --normalize_returns=True "
     "--with_wandb=True --wandb_tags multi obstacles "
@@ -18,10 +18,10 @@ SMALL_MODEL_CLI = QUAD_BASELINE_CLI + (
 )
 
 _experiment = Experiment(
-    "baseline_multi_drone",
+    "baseline_multi_drone_obstacle",
     SMALL_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
 
-RUN_DESCRIPTION = RunDescription("quad_multi_baseline", experiments=[_experiment])
+RUN_DESCRIPTION = RunDescription("quad_multi_baseline_obstacle", experiments=[_experiment])
 # python -m sample_factory.runner.run --run=sf_examples.swarm_rl_examples.runs.multi_drone --runner=processes --max_parallel=1 --pause_between=1 --experiments_per_gpu=4 --num_gpus=1
