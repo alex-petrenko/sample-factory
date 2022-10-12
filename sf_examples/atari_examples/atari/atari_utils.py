2e5072b14a8693c434c2b57612bcdfa2d44820e0
Edward Beeching <edbeeching@gmail.com>
2022-10-12 12:39:08 +0200

    added env pool support in batched mode

--- sf_examples/atari_examples/atari/atari_utils.py
+++ sf_examples/atari_examples/atari/atari_utils.py
@@ -2,8 +2,11 @@ from typing import Optional
 
 import gym
 
-from sample_factory.envs.env_wrappers import (
+from sample_factory.envs.env_wrappers import (  # For standard gym envs; For envpool envs
+    BatchedRecordEpisodeStatistics,
     ClipRewardEnv,
+    EnvPoolResetFixWrapper,
+    EnvPoolTo5Tuple,
     EpisodicLifeEnv,
     FireResetEnv,
     MaxAndSkipEnv,
@@ -15,24 +18,38 @@ ATARI_W = ATARI_H = 84
 
 
 class AtariSpec:
-    def __init__(self, name, env_id, default_timeout=None):
+    def __init__(self, name, env_id, envpool_id, default_timeout=None):
         self.name = name
         self.env_id = env_id
+        self.envpool_id = envpool_id
         self.default_timeout = default_timeout
         self.has_timer = False
 
 
+# Note NoFrameskip-v4 in gym[atari] is the same game configuration as -v5 in envpool
 ATARI_ENVS = [
-    AtariSpec("atari_montezuma", "MontezumaRevengeNoFrameskip-v4", default_timeout=18000),
-    AtariSpec("atari_pong", "PongNoFrameskip-v4"),
-    AtariSpec("atari_qbert", "QbertNoFrameskip-v4"),
-    AtariSpec("atari_breakout", "BreakoutNoFrameskip-v4"),
-    AtariSpec("atari_spaceinvaders", "SpaceInvadersNoFrameskip-v4"),
-    AtariSpec("atari_asteroids", "AsteroidsNoFrameskip-v4"),
-    AtariSpec("atari_gravitar", "GravitarNoFrameskip-v4"),
-    AtariSpec("atari_mspacman", "MsPacmanNoFrameskip-v4"),
-    AtariSpec("atari_seaquest", "SeaquestNoFrameskip-v4"),
-    AtariSpec("atari_beamrider", "BeamRiderNoFrameskip-v4"),
+    AtariSpec("atari_montezuma", "MontezumaRevengeNoFrameskip-v4", "MontezumaRevenge-v5", default_timeout=18000),
+    AtariSpec("atari_pong", "PongNoFrameskip-v4", "Pong-v5"),
+    AtariSpec(
+        "atari_qbert",
+        "QbertNoFrameskip-v4",
+        "Qbert-v5",
+    ),
+    AtariSpec("atari_breakout", "BreakoutNoFrameskip-v4", "Breakout-v5"),
+    AtariSpec("atari_spaceinvaders", "SpaceInvadersNoFrameskip-v4", "SpaceInvaders-v5"),
+    AtariSpec("atari_asteroids", "AsteroidsNoFrameskip-v4", "Asteroids-v5"),
+    AtariSpec(
+        "atari_gravitar",
+        "GravitarNoFrameskip-v4",
+        "Gravitar-v5",
+    ),
+    AtariSpec("atari_mspacman", "MsPacmanNoFrameskip-v4", "MsPacman-v5"),
+    AtariSpec(
+        "atari_seaquest",
+        "SeaquestNoFrameskip-v4",
+        "Seaquest-v5",
+    ),
+    AtariSpec("atari_beamrider", "BeamRiderNoFrameskip-v4", "BeamRider-v5"),
 ]
 
 
@@ -45,6 +62,19 @@ def atari_env_by_name(name):
 
 def make_atari_env(env_name, cfg, env_config, render_mode: Optional[str] = None):
     atari_spec = atari_env_by_name(env_name)
+    env = envpool.make(
+        atari_spec.envpool_id,
+        env_type="gym",
+        num_envs=cfg.env_agents,
+        reward_clip=True,
+        episodic_life=True,
+        max_episode_steps=18000,
+    )  # TODO set max_episode steps from spec
+    env = EnvPoolResetFixWrapper(env)
+    env = BatchedRecordEpisodeStatistics(env, num_envs=cfg.env_agents)
+    env = EnvPoolTo5Tuple(env)
+    env.num_agents = cfg.env_agents
+    return env
 
     env = gym.make(atari_spec.env_id, render_mode=render_mode)
 
