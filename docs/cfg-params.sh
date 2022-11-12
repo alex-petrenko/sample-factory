fa6abfe0c6bc82ea14da289b00220e940366efd8
Andrew Zhang <gnahzwerdna@gmail.com>
2022-11-12 04:38:35 +0000

    typo fix 2

--- docs/cfg-params.sh
+++ docs/cfg-params.sh
@@ -8,11 +8,10 @@ cat << EOF > $FILE
 The command line arguments / config parameters for training using Sample Factory can be found by running your training script with the \`--help\` flag. 
 The list of config parameters below was obtained from running \`python -m sf_examples.train_gym_env --env=CartPole-v1 --help\`. These params can be used in any environment.
 Other environments may have other custom params than can be viewed with \`--help\`
+
 \`\`\`
 EOF
 
 python -m sf_examples.train_gym_env --env=CartPole-v1 --help >> $FILE
 
-cat << EOF >> $FILE
-\`\`\`
-EOF 
+echo \`\`\` >> $FILE
