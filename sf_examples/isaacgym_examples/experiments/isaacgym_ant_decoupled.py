4454a619f47498b27b755e2a778d99a22b0e1e9c
Aleksei Petrenko <petrenko@usc.edu>
2022-12-07 01:33:50 -0800

    Experiment

--- sf_examples/isaacgym_examples/experiments/isaacgym_ant_decoupled.py
+++ sf_examples/isaacgym_examples/experiments/isaacgym_ant_decoupled.py
@@ -3,27 +3,28 @@ from sf_examples.isaacgym_examples.experiments.isaacgym_runs import base_cli, vs
 
 _params = ParamGrid(
     [
-        ("seed", [1111, 2222, 3333, 4444]),
-        (("use_rnn", "recurrence"), ([True, 16])),
-        ("num_epochs", [4, 6]),
+        ("seed", [1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888]),
+        ("env", ["Ant", "Humanoid"]),
+        (("use_rnn", "recurrence"), ([False, 1], [True, 16])),  # train recurrent and non-recurrent models
+        # ("num_epochs", [4, 6]),
         ("ppo_loss_decoupled", [True, False]),
-        # (("serial_mode", "async_rl"), ([True, False], [False, True])),
+        (("serial_mode", "async_rl"), ([False, True], [True, False])),
         # ("value_bootstrap", [True, False]),
         # (("use_rnn", "recurrence"), ([False, 1], [True, 16])),
     ]
 )
 
-vstr = f"{vstr}_decoupled"
+vstr = f"{vstr}_decoupled_v2"
 
-ant_cli = f" --env=Ant --train_for_env_steps=100000000 --with_wandb=True --wandb_tags ant {vstr}"
+ant_cli = f" --train_for_env_steps=100000000 --with_wandb=True --wandb_tags {vstr}"
 cli = base_cli + ant_cli
 
 _experiments = [
-    Experiment(f"ant_{vstr}", cli + f" --wandb_group=isaacgym_ant_sf2_{vstr}", _params.generate_params(False)),
+    Experiment(f"{vstr}", cli + f" --wandb_group=isaacgym_sf2_{vstr}", _params.generate_params(False)),
 ]
 
-RUN_DESCRIPTION = RunDescription(f"ant_{vstr}", experiments=_experiments)
+RUN_DESCRIPTION = RunDescription(f"{vstr}", experiments=_experiments)
 
 
-# Run locally: python -m sample_factory.launcher.run --run=sf_examples.isaacgym_examples.experiments.isaacgym_ant --backend=processes --max_parallel=2 --experiments_per_gpu=2 --num_gpus=1
-# Run on Slurm: python -m sample_factory.launcher.run --run=sf_examples.isaacgym_examples.experiments.isaacgym_ant --backend=slurm --slurm_workdir=./slurm_isaacgym --experiment_suffix=slurm --slurm_gpus_per_job=1 --slurm_cpus_per_gpu=16 --slurm_sbatch_template=./sample_factory/launcher/slurm/sbatch_timeout.sh --pause_between=1 --slurm_print_only=False
+# Run locally: python -m sample_factory.launcher.run --run=sf_examples.isaacgym_examples.experiments.isaacgym_ant_decoupled --backend=processes --max_parallel=2 --experiments_per_gpu=2 --num_gpus=1
+# Run on Slurm: python -m sample_factory.launcher.run --run=sf_examples.isaacgym_examples.experiments.isaacgym_ant_decoupled --backend=slurm --slurm_workdir=./slurm_isaacgym --experiment_suffix=slurm --slurm_gpus_per_job=1 --slurm_cpus_per_gpu=16 --slurm_sbatch_template=./sample_factory/launcher/slurm/sbatch_timeout.sh --pause_between=1 --slurm_print_only=False
