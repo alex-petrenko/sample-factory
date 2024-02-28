d30d4fa4e8265b233ab4522efdba966707f6b2b7
Anthony <26030673+im-ant@users.noreply.github.com>
2024-02-28 18:55:34 -0500

    Improve relative import for package resources

--- sf_examples/nethack/utils/wrappers/screen_image.py
+++ sf_examples/nethack/utils/wrappers/screen_image.py
@@ -19,9 +19,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+import importlib.resources
 import os
 from typing import Any, SupportsFloat
-import importlib.resources
 
 import cv2
 import gym
