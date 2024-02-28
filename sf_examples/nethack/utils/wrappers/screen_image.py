d8fe81595a7fd0eed4f1ee49462af87b283165ef
Anthony <26030673+im-ant@users.noreply.github.com>
2024-02-28 18:35:22 -0500

    Set font path relative to the sf_examples package so code can be instantiated from arbitrary current working directories

--- sf_examples/nethack/utils/wrappers/screen_image.py
+++ sf_examples/nethack/utils/wrappers/screen_image.py
@@ -21,6 +21,7 @@ SOFTWARE.
 
 import os
 from typing import Any, SupportsFloat
+import importlib.resources
 
 import cv2
 import gym
@@ -30,8 +31,7 @@ from nle import nethack
 from numba import njit
 from PIL import Image, ImageDraw, ImageFont
 
-SMALL_FONT_PATH = os.path.abspath("sf_examples/nethack/nethack_render_utils/Hack-Regular.ttf")
-
+SMALL_FONT_PATH = importlib.resources.files("sf_examples") / "nethack/nethack_render_utils/Hack-Regular.ttf"
 
 # Mapping of 0-15 colors used.
 # Taken from bottom image here. It seems about right
