8237d661eb0a6062dd8a58568fc3892a0df13eaa
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2025-01-24 18:33:18 +0100

    experiments with scale

--- train_configs/vit_rollout_scale_critic.py
+++ train_configs/vit_rollout_scale_critic.py
@@ -12,6 +12,7 @@ num_workers = 16
 
 # params for all exps
 config = {
+    "exp_tag": name,
     "env": "nethack_score",
     "run_script": "sf_examples.nethack.train_nethack",
     "train_for_env_steps": 100_000_000,
@@ -38,30 +39,38 @@ config = {
     "gae_lambda": 0.95,
     "value_loss_coeff": 0.5,
     "actor_critic_share_weights": False,
-    "critic_hidden_dim": 64,
-    "critic_mlp_dim": 128,
+    "critic_hidden_dim": 128,
+    "critic_mlp_dim": 256,
     "critic_depth": 2,
     "critic_heads": 8,
-    "actor_hidden_dim": 32,
-    "actor_mlp_dim": 64,
+    "actor_hidden_dim": 64,
+    "actor_mlp_dim": 128,
     "actor_depth": 1,
     "actor_heads": 8,
 }
 
 # params different between exps
-params_grid = [
-    {
-        "seed": list(range(1)),
-        "actor_hidden_dim": [actor_scale],
-        "actor_mlp_dim": [actor_scale * 2],
-        "actor_depth": [1, 2, 3],
-        "critic_hidden_dim": [critic_scale],
-        "critic_mlp_dim": [critic_scale * 2],
-        "critic_depth": [1, 2, 3],
-    }
-    for actor_scale in [32, 64, 128]
-    for critic_scale in [64, 128, 256]
-]
+params_grid = []
+expected_batch_size = 4096
+
+for rollout in [32, 64, 128]:
+    for target_batch_size in [32, 64, 128, 256, 512]:
+        batch_size = min(expected_batch_size, min(target_batch_size * rollout, expected_batch_size * 8))
+        batches_to_accumulate = max(1, (rollout * target_batch_size) // expected_batch_size)
+        for critic_scale in [1, 2, 4, 8]:
+            params_grid.append(
+                {
+                    "seed": list(range(1)),
+                    "rollout": [rollout],
+                    "batch_size": [batch_size],  # 32 * 512, 64 * 256, 128 * 128
+                    "gradient_accumulation_steps": [batches_to_accumulate],
+                    "target_batch_size": [target_batch_size],
+                    "actor_critic_share_weights": [False],
+                    "critic_hidden_dim": [64 * critic_scale],
+                    "critic_mlp_dim": [128 * critic_scale],
+                }
+            )
+
 
 experiments_list = create_experiments_helper(
     experiment_name=name,
