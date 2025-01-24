d28410164f042e1c8036b2ce5a0a2ff26f51c928
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2025-01-24 18:35:29 +0100

    remove old models

--- sf_examples/nethack/models/__init__.py
+++ sf_examples/nethack/models/__init__.py
@@ -1,11 +1,4 @@
-from sf_examples.nethack.models.chaotic_dwarf import ChaoticDwarvenGPT5
-from sf_examples.nethack.models.scaled import ScaledNet
-from sf_examples.nethack.models.simba import SimbaActorEncoder, SimbaCriticEncoder
+from sf_examples.nethack.models.vit import ViTActorEncoder, ViTCriticEncoder
 
-MODELS = [
-    ChaoticDwarvenGPT5,
-    ScaledNet,
-    SimbaActorEncoder,
-    SimbaCriticEncoder,
-]
+MODELS = [ViTActorEncoder, ViTCriticEncoder]
 MODELS_LOOKUP = {c.__name__: c for c in MODELS}
