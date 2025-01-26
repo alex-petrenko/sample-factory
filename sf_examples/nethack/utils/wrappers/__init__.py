c7cfbdfd83cc1bf22fae8633b5d9f9a9108eac8a
Bartek Cupiał <bartlomiej.cupial@gmail.com>
2025-01-26 05:50:27 +0000

    tile tty

--- sf_examples/nethack/utils/wrappers/__init__.py
+++ sf_examples/nethack/utils/wrappers/__init__.py
@@ -4,11 +4,13 @@ from sf_examples.nethack.utils.wrappers.no_progress_timeout import NoProgressTim
 from sf_examples.nethack.utils.wrappers.prev_actions import PrevActionsWrapper
 from sf_examples.nethack.utils.wrappers.screen_image import RenderCharImagesWithNumpyWrapperV2
 from sf_examples.nethack.utils.wrappers.task_rewards import TaskRewardsInfoWrapper
+from sf_examples.nethack.utils.wrappers.tile_tty import TileTTY
 from sf_examples.nethack.utils.wrappers.timelimit import NLETimeLimit
 
 __all__ = [
     PrevActionsWrapper,
     TaskRewardsInfoWrapper,
+    TileTTY,
     BlstatsInfoWrapper,
     NLETimeLimit,
     GymV21CompatibilityV0,
